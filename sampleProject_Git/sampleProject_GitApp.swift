415132a45bec34699cc780de4d4b56384b82eb12
blue0print <67566815+blue0print@users.noreply.github.com>
2022-11-14 23:30:33 -0800

    Initial Commit

---
+++ sampleProject_Git/sampleProject_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleProject_GitApp.swift
+//  sampleProject_Git
+//
+//  Created by Ismayil ADIGOZALZADA on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct sampleProject_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
